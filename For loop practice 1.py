b05ad47725086c0d66f1f85244e1cb2cb0d6182e
HarshitaB2604 <57848574+HarshitaB2604@users.noreply.github.com>
2019-12-03 12:11:33 -0800

    Create For loop practice 1.py

---
+++ For loop practice 1.py
@@ -0,0 +1,7 @@
+x = 10 #number of rows 
+
+print("SECOND")
+for i in range(x):
+    for j in range(x):# this loop determines the number of astriks in the row
+        print("*", end=" ")
+    print("")
