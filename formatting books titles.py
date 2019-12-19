e8e6afb2063464f26b102020bbb2dc0d19ce0a60
Harshita <57848574+HarshitaB2604@users.noreply.github.com>
2019-12-19 09:07:53 -0800

    Update and rename formatting books titles to formatting books titles.py

--- formatting books titles.py
+++ formatting books titles.py
@@ -3,29 +3,27 @@
   and stores it in a file it creats
 * then it print out each line with the book title in upper case along with the authors last name, first name capitilized 
 '''
-
 # Saving filepath to a variable
 # makes a smoother transition to the Sandbox
 filepath = "books.txt"
 
 # When finished copy all code after this line into the Sandbox
-
 # Open the file
 outFile = open (filepath, 'w') 
 
 # Write your program below
-title = input("Enter a title of a book. Enter STOP to stop")
-authorL = input("Enter the last name of the author")
-authorF = input("Enter the first name of the author")
+title = input("Enter a title of a book. Enter STOP to stop ")
+authorF = input("Enter the first name of the author ")
+authorL = input("Enter the last name of the author ")
 
 while(title != 'STOP'):
   #formats the tile and the authors name to store it in one line
   outFile.write(title.upper() + "\t" + authorL.capitalize() + ", " + authorF.capitalize() + "\n")
 
-  title = input("Enter a title of a book. Enter stop to stop")
+  title = input("Enter a title of a book. Enter STOP to stop ")
   if(title != 'STOP'):
-    authorL = input("Enter the last name of the author")
-    authorF = input("Enter the first name of the author")
+    authorF = input("Enter the first name of the author ")
+    authorL = input("Enter the last name of the author ")
 
 outFile.close()
 
@@ -36,3 +34,5 @@ line = inFile.readline()
 while(line):
   print(line)
   line = inFile.readline()
+
+inFile.close()
