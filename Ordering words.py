faf03a7366931fed2a1a209bc2dc57c2770750ad
Harshita <57848574+HarshitaB2604@users.noreply.github.com>
2020-01-06 12:08:40 -0800

    Update Ordering words.py

--- Ordering words.py
+++ Ordering words.py
@@ -1,11 +1,11 @@
-word1 = input("First word: ")
-word2 = input('Second word: ')
+word1 = input("First word: ").upper()
+word2 = input('Second word: ').upper()
 
 #tesing which word comes first alphabetically then 
 #prints the words in alphabetically order in upper case
 
 if(word1 < word2):  
-    print(str(word1.upper()) + "\n" + str(word2.upper()))
+    print(word1 + "\n" + word2)
 else:
-    print(str(word2.upper()) + "\n" + str(word1.upper()))
+    print(word2 + "\n" + word1)
    
