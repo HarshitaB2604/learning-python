781643da7958ddbd7a200240269e29afabfeaaae
Harshita <57848574+HarshitaB2604@users.noreply.github.com>
2020-01-07 11:32:48 -0800

    Create summing with a function.py
    The program takes in 3 numbers and prints their sum using a function.

---
+++ summing with a function.py
@@ -0,0 +1,11 @@
+def print_sum (n1, n2, n3):
+    sum = n1 + n2 + n3
+    print("The sum: " + str(sum))
+    
+    
+#Main
+num1 = int(input("Enter the 1st number: "))
+num2 = int(input("Enter the 2nd number: "))
+num3 = int(input("Enter the 3rd number: "))
+print_sum(num1, num2, num3)
+    
