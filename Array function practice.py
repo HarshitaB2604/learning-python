873223cda4d447bb5c2bd52bbfa7600a66328a84
Harshita <57848574+HarshitaB2604@users.noreply.github.com>
2020-02-14 11:38:56 -0800

    Update Array function practice.py

--- Array function practice.py
+++ Array function practice.py
@@ -5,7 +5,7 @@ def buildArray(ar, val):
   #takes in a number and builds and arrays with that many values generated from random numbers
 
   for i in range(val):
-    ar.append(random.randint(0, 99))
+    ar.append(random.randint(10, 99))
 
 #***********MAIN********
 array = []
