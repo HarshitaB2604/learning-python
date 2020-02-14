a385454f4ea89c1aabbed7a90ca6d642edcc90b8
Harshita <57848574+HarshitaB2604@users.noreply.github.com>
2020-02-14 11:41:52 -0800

    Update Array function practice.py

--- Array function practice.py
+++ Array function practice.py
@@ -7,11 +7,22 @@ def buildArray(ar, val):
   for i in range(val):
     ar.append(random.randint(10, 99))
 
+def sumArray(ar):
+  sum = 0
+  for i in range(len(ar)-1):
+    sum = sum + ar[i]
+
+  return sum  
+
 #***********MAIN********
 array = []
 
 #asks for the number of values
 num = int(input("How many values to add to the array: \n"))
 
+#builds the array
 buildArray(array, num)
 print(array)
+
+#find the sum
+print("Total: " + str(sumArray(array)))
