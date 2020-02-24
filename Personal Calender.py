f8a49db53db1b3df79663f2e5363f9280cf4bb0b
Harshita <57848574+HarshitaB2604@users.noreply.github.com>
2020-02-24 11:16:46 -0800

    Update Personal Calender.py

--- Personal Calender.py
+++ Personal Calender.py
@@ -1,16 +1,65 @@
 #****************************FUNCTIONS*************************
 def addEvent(name, month, day, year):
   #adds an event to the personal calender
+  nameInput = input("What is the event: ")
+  monthInput = int(input("What is the month (number): "))
+  dayInput = int(input("What is the date: "))
+  yearInput = int(input("What is the year: "))
 
+  #checks if the month number is valid
+  if(monthInput < 1 or monthInput > 12):
+    monthInput = 1
+  
+  #checks if the day input is invalid
+  #finds the number of days in the months entered
+  daysInMonth = checkDays(monthInput, leap_year(yearInput))
+  if(dayInput > daysInMonth):
+    dayInput = 1
 
+  #adds the event to the arrays
+  name.append(nameInput)
+  month.append(monthName(monthInput))
+  day.append(dayInput)
+  year.append(yearInput)
+
+'''
 def printEvents(arrayName, arrayMonth, arrayDay, arrayYear):
   #takes in the four arrays and prints thier contents
-
+  print()
 def printMonth(monthNum, arrayName, arrayMonth, arrayDay, arrayYear):
   #prints the events in a specific month
-
+  print("")
+'''
 
 #other supporting functions
+def monthName(num):
+  if(num == 1):
+    month = "January"
+  elif(num == 2):
+    month = "February"
+  elif(num == 3):
+    month = "March"
+  elif(num == 4):
+    month = "April"
+  elif(num == 5):
+    month = "May"
+  elif(num == 6):
+    month = "June"
+  elif(num == 7):
+    month = "July"
+  elif(num == 8):
+    month = "August"
+  elif(num == 9):
+    month = "September"
+  elif(num == 10):
+    month = "October"
+  elif(num == 11):
+    month = "November"
+  else:
+    month = "December"
+
+  return month
+
 def leap_year(y):
   #determins if the year is a leap year
 
@@ -51,13 +100,14 @@ eventYear = []
 #Code
 #add the events
 event = ""
-while(event != "no"):
+while(event != "NO"):
   addEvent(eventName, eventMonth, eventDay, eventYear)
-  event = "Do you want to enter another date? no to stop."
+  event = "Do you want to enter another date? NO to stop."
 
 #print the events
 print("******************** List of Events ********************")
 
 #print events in a specific month
-print()
+monthNum = int(input("What month would you like to see? (Enter the month number) "))
+print("********** Events in " + monthName(monthNum) + " **********")
 
