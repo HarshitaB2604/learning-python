27456f0c272b1c33d0c40a202269966f44f65836
Harshita <57848574+HarshitaB2604@users.noreply.github.com>
2020-04-10 11:36:19 -0700

    Update making my first class.py

--- making my first class.py
+++ making my first class.py
@@ -4,14 +4,20 @@ class Rectangle:
     def __init__(self, length = 0 , width = 0):
         self.length = length
         self.width = width
+        
     #method will calculate the area of the reactangle   
     def get_area(self):
         return self.length*self.width
+        
+    #method will calculate the perimeter of the reactangle
+    def get_perimeter(self):
+        return self.length*2 + self.width*2
 
 
 rect = Rectangle(4, 6)
 print(rect.length)
 print(rect.width)
 print("Area: " + str(rect.get_area()))
+print("Perimeter: " + str(rect.get_perimeter()))
 
 rect2 = Rectangle()
