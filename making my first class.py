4cb0e9df29d8a6771f8841e9cbf0cc95bb98b1e7
Harshita <57848574+HarshitaB2604@users.noreply.github.com>
2020-04-10 11:34:16 -0700

    Update making my first class.py

--- making my first class.py
+++ making my first class.py
@@ -1,11 +1,17 @@
 class Rectangle:
-    
+
     #method to assign the lenght and width of the reactangle
     def __init__(self, length = 0 , width = 0):
         self.length = length
         self.width = width
-        
-        
+    #method will calculate the area of the reactangle   
+    def get_area(self):
+        return self.length*self.width
+
+
 rect = Rectangle(4, 6)
 print(rect.length)
 print(rect.width)
+print("Area: " + str(rect.get_area()))
+
+rect2 = Rectangle()
